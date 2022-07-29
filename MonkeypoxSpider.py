70b2e2aa5ffac13ebecf4fd6660cd68465b77a00
David J Kaftan <kaftand@gmail.com>
2022-07-29 10:07:08 -0400

    Update MonkeypoxSpider.py

--- MonkeypoxSpider.py
+++ MonkeypoxSpider.py
@@ -9,7 +9,7 @@ class MonkeypoxSpider(scrapy.Spider):
         time = self.crawler.stats.get_value("start_time")
         for title  in response.xpath('//h2'):
             if title.xpath("./text()")[0].extract() == "Cases in NYC":
-                rawText = title.xpath("../p[1]/text()")[0].extract()
+                rawText = title.xpath("../p[2]/text()")[0].extract()
                 dateLine = re.search("(?i)(June|July|August|Aug\.|Aug|September|Sept\.|Sept|October|Oct\.|Oct|November|Nov\.|Nov|December|Dec\.|Dec|January|Jan\.|Jan|February|Feb\.|Feb|March|Mar\.|Mar|April|Apr\.|Apr|May) [0-9]{1,2}",
                           rawText)
                 date = dateLine[0] if dateLine else dateLine
